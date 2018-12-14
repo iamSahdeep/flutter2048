e44b08ab6fea0736bbac2c495bd8bdf6965f7589
Sahdeep Singh <sahdeepsingh98@gmail.com>
2018-12-14 21:50:19 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2048/webview.dart';
+
+void main() => runApp(MaterialApp(
+  home: WebViewExample(),
+  debugShowCheckedModeBanner: false,
+));
---
+++ lib/webview.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  FlutterWebviewPlugin flutterWebviewPlugin = FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged wvs) {
+      print(wvs.type);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: 'file:///android_asset/index.html',
+      allowFileURLs: true,
+      withZoom: false,
+    );
+  }
+}
